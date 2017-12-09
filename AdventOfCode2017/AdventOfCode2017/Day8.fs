58bffac68a72cd2d597c494f63bd1ce90154da7f
blainne <grzegorz.slawecki@gmail.com>
2017-12-09 20:46:59 +0100

    .

--- AdventOfCode2017/AdventOfCode2017/Day8.fs
+++ AdventOfCode2017/AdventOfCode2017/Day8.fs
@@ -2,36 +2,33 @@ module Day8
 open System.IO
 open System
 
-type Operation = | Incr of string*int | Decr of string*int
-type Condition = 
-    | Eq of string*int 
-    | Neq of string*int 
-    | Leq of string*int
-    | Geq of string*int
-    | Lt of string*int
-    | Gt of string*int
+type Operation = | Incr | Decr
+type Condition = | Eq | Neq | Leq | Geq | Lt | Gt
+type Argument = | Reg of string | Const of int
+type Expr<'a> = {Op: 'a; LeftArg: Argument; RightArg: Argument}
+type Instruction = {Op: Expr<Operation>; Cond: Expr<Condition>}
 
-type Instruction = {Op: Operation; Cond: Condition}
+let parseErr () = failwith "Unexpected input"
 
 let parseOperation (tokens:string[]) =
         let registryName = tokens.[0]
         let amount = int tokens.[2]
-        match tokens.[1] with
-        | "inc" -> Incr(registryName, amount)
-        | "dec" -> Decr(registryName, amount)
-        | _     -> failwith "Unexpected input"
+        let op = 
+            match tokens.[1] with
+            | "inc" -> Incr | "dec" -> Decr | _ -> parseErr()
+        {Op = op; LeftArg = Reg(registryName); RightArg = Const(amount)}
 
 let parseCondition (tokens:string[]) =
         let registryName = tokens.[4]
         let amount = int tokens.[6]
-        match tokens.[5] with
-        | "<" -> Lt(registryName, amount)
-        | ">" -> Gt(registryName, amount)
-        | "==" -> Eq(registryName, amount)
-        | "!=" -> Neq(registryName, amount)
-        | ">=" -> Geq(registryName, amount)
-        | "<=" -> Leq(registryName, amount)
-        | _     -> failwith "Unexpected input"
+        let cond = 
+            match tokens.[5] with
+            | "<" -> Lt | ">" -> Gt | "==" -> Eq
+            | "!=" -> Neq | ">=" -> Geq | "<=" -> Leq
+            | _ -> parseErr()
+
+        {Op = cond; LeftArg = Reg(registryName); RightArg = Const(amount)}
+        
 let parseRow (str:string) =
     let tokens = str.Split([|" "|], StringSplitOptions.RemoveEmptyEntries)
     { Op = parseOperation tokens; Cond = parseCondition tokens }
@@ -46,13 +43,13 @@ let readAndInit key map =
     then (map.[key], map)
     else (0, Map.add key 0 map) 
 
-let runInstruction regs instr =
-    let (condRegistry, condVal) = instr.Cond
+// let runInstruction regs instr =
+//     let (condRegistry, condVal) = instr.Cond
     
 
-let execute instructions =
-    instructions
-    |> List.fold runInstruction (Map[])
+// let execute instructions =
+//     instructions
+//     |> List.fold runInstruction (Map[])
 
 
 let testInput = "./AdventOfCode2017/AdventOfCode2017/inputs/Day8-test.txt"
